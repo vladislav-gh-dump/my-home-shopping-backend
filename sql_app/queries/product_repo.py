4c882cacd03d10fa98d74033785e942979227be9
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-09-09 01:44:07 +0300

    create product queries

---
+++ sql_app/queries/product_repo.py
@@ -0,0 +1,67 @@
+from typing import List, Optional
+from sqlalchemy import select, update
+from sqlalchemy.exc import SQLAlchemyError
+
+from sql_app.db import NewSession
+from sql_app.models import ORM_Product
+from sql_app.schemas import S_Product, S_ProductCreate, S_ProductUpdate
+
+
+class ProductRepo:
+    
+    @staticmethod
+    async def read_all(skip: int = 0, limit: int = 20) -> List[S_Product]:
+      try:
+        async with NewSession() as session:
+          result = await session.execute(
+              select(ORM_Product)
+              .limit(limit)
+              .offset(skip)
+          )
+          return result.scalars().all()
+      except SQLAlchemyError as e:
+        print(f"Database error occurred: {e}")
+        return []
+
+    @staticmethod
+    async def read_one(product_id: int) -> Optional[S_Product]:
+      try:
+        async with NewSession() as session:
+          result = await session.execute(
+              select(ORM_Product)
+              .where(ORM_Product.id == product_id)
+          )
+          return result.scalar_one_or_none()
+      except SQLAlchemyError as e:
+        print(f"Database error occurred: {e}")
+        return None
+
+    @staticmethod
+    async def create(product: S_ProductCreate) -> Optional[int]:
+      try:
+        async with NewSession() as session:
+          data = product.model_dump()
+          new_product = ORM_Product(**data)
+          session.add(new_product)
+          await session.flush()
+          await session.commit()
+          return new_product.id
+      except SQLAlchemyError as e:
+        print(f"Database error occurred: {e}")
+        return None
+
+    @staticmethod
+    async def update(product_id: int, product: S_ProductUpdate) -> Optional[int]:
+      try:
+        async with NewSession() as session:
+          data = product.model_dump()
+          result = await session.execute(
+              update(ORM_Product)
+              .where(ORM_Product.id == product_id)
+              .values(**data)
+          )
+          await session.commit()
+          return result.rowcount 
+      except SQLAlchemyError as e:
+        print(f"Database error occurred: {e}")
+        return None
