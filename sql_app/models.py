0fda32e9e90490a64993998cf7ed56981377d004
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-09-09 01:06:37 +0300

    edit product_category_id

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,9 +1,6 @@
 from datetime import datetime, timezone
-from typing import List
-from sqlalchemy import (
-  ForeignKey,
-  func
-)
+from typing import List, Optional
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import (
   DeclarativeBase, 
   Mapped, mapped_column,
@@ -35,6 +32,6 @@ class ORM_Product(ORM_Base):
   created_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
   updated_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
   
-  product_category_id: Mapped[int]                  = mapped_column(ForeignKey("product_category.id"))
+  product_category_id: Mapped[Optional[int]]         = mapped_column(ForeignKey("product_category.id"))
   product_category:    Mapped["ORM_ProductCategory"] = relationship(back_populates="products") 
   
