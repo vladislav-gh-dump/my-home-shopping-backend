dceeca0148cd2b4a7cd90080363b0276cc6a3136
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-09-09 01:07:38 +0300

    edit schemas

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,15 +1,15 @@
-import datetime
-from typing import List
+from datetime import datetime 
+from typing import List, Optional
 from pydantic import BaseModel, Field
 
 
 class S_ProductBase(BaseModel):
   name: str = Field(min_length=1, max_length=30)
-  product_category_id: int | None = None
-  
+  product_category_id: Optional[int] = None
+
 class S_ProductCreate(S_ProductBase):
   pass
-  
+
 class S_ProductUpdate(S_ProductBase):
   pass
 
@@ -19,15 +19,15 @@ class S_Product(S_ProductBase):
   updated_at: datetime
   
   class Config:
-    orm_mode = True
+    from_attributes = True
 
 
 class S_ProductCategoryBase(BaseModel):
   name: str = Field(min_length=1, max_length=30)
-  
+
 class S_ProductCategoryCreate(S_ProductCategoryBase):
   pass
-  
+
 class S_ProductCategoryUpdate(S_ProductCategoryBase):
   pass
 
@@ -36,6 +36,6 @@ class S_ProductCategory(S_ProductCategoryBase):
   created_at: datetime
   updated_at: datetime
   products: List[S_Product] = []
-  
+
   class Config:
-    orm_mode = True
+    from_attributes = True
