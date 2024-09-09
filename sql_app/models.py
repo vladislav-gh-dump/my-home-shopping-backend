3ff349d28fa75568f678ff1f1122fa91308a6c4d
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-09-09 03:24:00 +0300

    edit fields created_at and updated_at

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import List, Optional
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import (
   DeclarativeBase, 
   Mapped, mapped_column,
@@ -18,8 +18,8 @@ class ORM_ProductCategory(ORM_Base):
   
   id:         Mapped[int]      = mapped_column(primary_key=True)
   name:       Mapped[str]      = mapped_column(unique=True)
-  created_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
-  updated_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+  created_at: Mapped[datetime] = mapped_column(default=func.now())
+  updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
   
   products: Mapped[List["ORM_Product"]] = relationship(back_populates="product_category")
                                      
@@ -29,8 +29,8 @@ class ORM_Product(ORM_Base):
   
   id:         Mapped[int]      = mapped_column(primary_key=True)
   name:       Mapped[str]      = mapped_column(unique=True)
-  created_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
-  updated_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+  created_at: Mapped[datetime] = mapped_column(default=func.now())
+  updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
   
   product_category_id: Mapped[Optional[int]]         = mapped_column(ForeignKey("product_category.id"))
   product_category:    Mapped["ORM_ProductCategory"] = relationship(back_populates="products") 
