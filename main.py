ed3bbeba5d63fa35dc4f6727c25de62c7d1f22f6
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-09-09 03:29:36 +0300

    add cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from sql_app.queries.structure_db import create_tables, delete_tables
 
@@ -22,5 +23,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(products_router)
 app.include_router(product_categories_router)
