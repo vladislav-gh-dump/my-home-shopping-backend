af5b02a7d151069bea60fe2970cf8a19fb60bfdd
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-09-10 20:11:51 +0300

    add new parent class Config

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,7 +1,17 @@
 from datetime import datetime 
 from typing import List, Optional
 from pydantic import BaseModel, Field
+from pydantic.alias_generators import to_camel
 
+
+class S_ConfigBase(BaseModel):
+  class Config: 
+    alias_generator = to_camel
+    populate_by_name = True
+    from_attributes = True
+
+
+# Schemas of product 
 
 class S_ProductBase(BaseModel):
   name: str = Field(min_length=1, max_length=30)
@@ -13,14 +23,13 @@ class S_ProductCreate(S_ProductBase):
 class S_ProductUpdate(S_ProductBase):
   pass
 
-class S_Product(S_ProductBase):
+class S_Product(S_ProductBase, S_ConfigBase):
   id: int
   created_at: datetime
   updated_at: datetime
-  
-  class Config:
-    from_attributes = True
 
+
+# Schemas of product category
 
 class S_ProductCategoryBase(BaseModel):
   name: str = Field(min_length=1, max_length=30)
@@ -31,11 +40,8 @@ class S_ProductCategoryCreate(S_ProductCategoryBase):
 class S_ProductCategoryUpdate(S_ProductCategoryBase):
   pass
 
-class S_ProductCategory(S_ProductCategoryBase):
+class S_ProductCategory(S_ProductCategoryBase, S_ConfigBase):
   id: int
   created_at: datetime
   updated_at: datetime
   products: List[S_Product] = []
-
-  class Config:
-    from_attributes = True
